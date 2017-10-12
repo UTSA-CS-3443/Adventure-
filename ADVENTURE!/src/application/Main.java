bbabb166688c65c9397c38826ca3bcf11481f30b
JAM10113 <00jam00@gmail.com>
2017-10-12 14:30:06 -0500

    moved code for game screen to GameScreen.

--- ADVENTURE!/src/application/Main.java
+++ ADVENTURE!/src/application/Main.java
@@ -3,17 +3,28 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import screens.GameScreen;
 import screens.MainMenu;
 
 //work in progress screens should be loaded in from screens classes by completion
 public class Main extends Application {
+	
+	private Scene scene;
+	//BorderPane g = GameScreen.getRoot();
+	GameScreen gs;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		primaryStage.setTitle("ADVENTURE!");
+		gs = new GameScreen(primaryStage);
+		
+		/*
 		try {
-			BorderPane root = FXMLLoader.load(getClass().getResource("GameScreen.fxml"));
+			BorderPane root = FXMLLoader.load(getClass().getResource("/screens/GameScreen.fxml"));
 			Scene scene = new Scene(root,900,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -21,6 +32,11 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		//*/
+		
+		scene = gs.getScene();
+		primaryStage.show();
+		
 	}
 	
 	public static void main(String[] args) {
--- ADVENTURE!/src/screens/GameScreen.java
+++ ADVENTURE!/src/screens/GameScreen.java
@@ -1,9 +1,34 @@
 package screens;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class GameScreen {
 
-	public GameScreen() {
-		// TODO Auto-generated constructor stub
+	private static Scene scene;
+	private static BorderPane root;
+	
+	public GameScreen(Stage stage) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("GameScreen.fxml"));
+			scene = new Scene(root,900,600);
+			scene.getStylesheets().add(getClass().getResource("../application/application.css").toExternalForm());
+			stage.setScene(scene);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static BorderPane getRoot()
+	{
+		return root;
+	}
+	
+	public static Scene getScene()
+	{
+		return scene;
 	}
 
 }
