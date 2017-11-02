91471d38342d60e9c706da0878de062d185dd44b
wpm361 <wpm361@N0000A163.cs.utsarr.net>
2017-11-02 11:46:40 -0500

    enemies

---
+++ ADVENTURE!/src/Enemies/Goblin.java
@@ -0,0 +1,50 @@
+package Enemies;
+
+import objects.Enemy;
+
+public class Goblin extends Enemy {
+	
+	String name;
+	
+	public Goblin(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getHp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setHp(int hp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int[] getStats() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStats(int[] stats) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
