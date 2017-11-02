da1f3de228ff7c6500f5235552bd5551d2fbd808
wpm361 <wpm361@N0000A163.cs.utsarr.net>
2017-11-02 11:37:57 -0500

    more npc's

---
+++ ADVENTURE!/src/npc/Guide.java
@@ -0,0 +1,50 @@
+package npc;
+
+import objects.NPC;
+
+public class Guide extends NPC {
+	
+	String name;
+	
+	public Guide(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getHp() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setHp(int hp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int[] getStats() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStats(int[] stats) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
