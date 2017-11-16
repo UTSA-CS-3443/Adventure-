0062d3fc8b6982c585c3fa6cfe620f3dac2b895c
JAM10113 <00jam00@gmail.com>
2017-11-16 11:46:33 -0600

    Fixing merge conflicts

--- ADVENTURE!/src/events/Event.java
+++ ADVENTURE!/src/events/Event.java
@@ -1,8 +1,11 @@
 package events;
 
 import java.util.HashMap;
+import java.util.Random;
 
+import application.Main;
 import locations.Attribute;
+import objects.Player;
 
 public class Event	{
 
@@ -11,11 +14,16 @@ public class Event	{
 	private boolean completed;
 	HashMap<String, String> choices = new HashMap<>();
 	HashMap<String, Integer> stats = new HashMap<>();
+	private int hp;
+	Player player;
 	
 	public Event() {
 		eventName = "";
 		eventDesc = "";
 		completed = false;
+		player = Main.gs.player.getPlayer();
+		stats = player.getStats();
+		hp = player.getHp();
 	}
 	
 	public String getName()
@@ -45,6 +53,7 @@ public class Event	{
 	
 	public void outcomes(String text)
 	{
+		HashMap<String, Integer> playerStats = new HashMap<>();
 		String outcome = choices.get(text);
 		String[] outcomeParser = outcome.split("~");
 		String[] operation = null;
@@ -52,15 +61,41 @@ public class Event	{
 		for(int i=0;i<outcomeParser.length;i++)
 		{
 			operation = outcomeParser[i].split(" ");
-		}
-		
-		for(int j=0;j<operation.length;j++)
-		{
-			if(operation[0].equals("STR|PER|INT|AGI|LUC"))
+			for(int j=0;j<operation.length;j++)
 			{
-				
+				if(operation[0].equals("STR|PER|INT|AGI|LUC"))
+				{
+					if(operation[1].equals("+"))
+					{
+						player.setStats(operation[0], stats.get(operation[0]) + Integer.valueOf(operation[3]));
+					}
+					else if(operation[0].equals("-"))
+					{
+						player.setStats(operation[0], stats.get(operation[0]) - Integer.valueOf(operation[3]);
+					}
+					else
+						break;
+				}
+				else if(operation[0].equals("HP"))
+				{
+					if(operation[1].equals("+"))
+					{
+						player.setHp(hp + Integer.valueOf(operation[3]));
+					}
+					else if(operation[1].equals("-"))
+					{
+						player.setHp(hp - Integer.valueOf(operation[3]));
+					}
+					else
+						break;
+				}
+				else if(operation[0].equals("ROLL"))
+				{
+					int die = (int) (Math.random()*20+1) ;
+				}
 			}
 		}
+		
 		
 	}
 	
