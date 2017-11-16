1ad7c63b6972554e34d6968c0a3d9ecffdb8cafc
TRUTSA <nmt848@my.utsa.edu>
2017-11-16 12:18:49 -0600

    Fixed unnecessary implements

--- ADVENTURE!/src/npc/ShopKeeper.java
+++ ADVENTURE!/src/npc/ShopKeeper.java
@@ -14,34 +14,4 @@ public class ShopKeeper extends NPC {
 		super();
 		this.name = name;
 	}
-	
-	@Override
-	public String getName() {
-		return this.name;
-	}
-
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public int getHp() {
-		return this.hp;
-	}
-
-	@Override
-	public void setHp(int hp) {
-		this.hp = hp;
-	}
-
-	@Override
-	public HashMap<String, Integer> getStats() {
-		return this.stats;
-	}
-
-	@Override
-	public void setStats(String s, int i) {
-		stats.put(s, i);
-	}
 }
