f6491753a76a9c066dedd07452d6744d37e9f508
JAM10113 <00jam00@gmail.com>
2017-11-16 23:59:04 -0600

    adding event popups

---
+++ ADVENTURE!/src/controller/EventController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextFlow;
+
+public class EventController extends AnchorPane{
+	@FXML VBox CHOICES;
+	@FXML TextFlow TITLE;
+	@FXML TextFlow DESC;
+
+}
